1b823a641b578b805d1513122ebd7306571d3e0b
coder <kard3ti@gmail.com>
2021-01-27 21:51:11 -0500

    Create authentication in views snippets app

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,38 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+from .serializers import UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet
     """
+    permision_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
+    permision_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset= Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
