42694fa48e800161c1727bf937e36d32e632c9e2
coder <kard3ti@gmail.com>
2021-01-30 23:57:43 -0500

    Create view from path root snippets and create view snippet highlight

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import (SnippetList, SnippetDetail, UserList, UserDetail)
+from snippets.views import (SnippetList, SnippetDetail, 
+                            UserList, UserDetail, api_root,
+                            SnippetHighlight)
 
 urlpatterns = [
+    path('', api_root),
     path('snippets/', SnippetList.as_view()),
     path('snippets/<int:pk>', SnippetDetail.as_view()),
+    path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view()),
     path('users/', UserList.as_view()),
     path('users/<int:pk>/', UserDetail.as_view()),
 ]
